b57a1859caff56193dd8100407402271817d32e1
shravanmeena <shravanmeena47@gmail.com>
2023-02-13 01:59:53 +0530

    Update about me section for my introduction

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 export const Config = {
   API_URL: 'https://api.giphy.com/v1/gifs',
   API_KEY: 'lZgItPO8cwRB5Egp0J26TdepKKkXnPA4', //GIF API KEY / TOKEN
+  PORTFOLIO_LINK: 'https://shravanmeena.netlify.app/',
 };
--- src/screens/aboutMe/index.js
+++ src/screens/aboutMe/index.js
@@ -1,5 +1,9 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
+import {Linking, StyleSheet} from 'react-native';
+
+// contants
+import {Config} from '@config';
 
 // ui - components
 import {Center, Container, Heading, Description, Base, Button} from '@ui';
@@ -7,11 +11,16 @@ import {Center, Container, Heading, Description, Base, Button} from '@ui';
 // styles
 import {Colors} from '@styles';
 import {SCALE_18} from '@styles/spacing';
+import {SCALE_12, SCALE_8} from '@styles/spacing';
 
 export default function AboutMe() {
   const {goBack} = useNavigation();
   const backHandler = () => {
     goBack();
+  };
+
+  const openPortfolio = () => {
+    Linking.openURL(Config.PORTFOLIO_LINK);
   };
 
   return (
@@ -34,10 +43,30 @@ export default function AboutMe() {
             I've been coding for 4 years with making full stack Projects from
             scratch level to industry level. Have experienced founder journey as
             CTO of Saas startup. Currently working with a Gaming platform
-            startup, made it from scratch to now having 1 million+ users.
+            startup, made it from scratch to now having 1.5 million+ users.
           </Description>
+
+          <Button
+            onPress={openPortfolio}
+            text_style={{
+              color: Colors.WHITE,
+            }}
+            props_styles={styles.btnPortfolio}>
+            MY PORTFOLIO
+          </Button>
         </Center>
       </Container>
     </Base>
   );
 }
+
+const styles = StyleSheet.create({
+  btnPortfolio: {
+    borderWidth: SCALE_8,
+    borderColor: Colors.WARNING,
+    backgroundColor: Colors.ALERT,
+    paddingHorizontal: SCALE_12,
+    paddingVertical: SCALE_8,
+    marginVertical: SCALE_18 * 3,
+  },
+});
