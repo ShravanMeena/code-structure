e86cfd9c24cfe173e2894fd48bd63ff462567511
shravanmeena <shravanmeena47@gmail.com>
2023-02-12 13:12:31 +0530

    module resolver - unknow bug fix

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,18 @@
-const presets = ['module:metro-react-native-babel-preset'];
-const plugins = [];
-
-const moduleResolver = {
-  plugins: [
-    [
-      'module-resolver',
-      {
-        // root: ['./src'],
-        alias: {
-          components: './src/components',
-          redux: './src/redux',
-        },
-      },
-    ],
-  ],
-};
-
 module.exports = {
-  presets,
-  plugins,
-  moduleResolver,
+  presets: ['module:metro-react-native-babel-preset'],
+  moduleResolver: {
+    plugins: [
+      [
+        'module-resolver',
+        {
+          // root: ['./src'],
+          alias: {
+            screens: './src/screens',
+            components: './src/components',
+            redux: './src/redux',
+          },
+        },
+      ],
+    ],
+  },
 };
